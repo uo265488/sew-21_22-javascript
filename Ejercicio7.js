b5dfe0952f4ada543ce020f42399c393ecf258a8
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-19 09:35:04 +0100

    Add files via upload

---
+++ Ejercicio7.js
@@ -0,0 +1,44 @@
+
+class GestorWeb {
+
+    constructor() {
+
+    }
+
+    ocultarParrafos() {
+        $("p").hide();
+    }
+
+    mostrarParrafos() {
+        $("p").show();
+    }
+
+    ocultarTabla() {
+        $("table").hide();
+    }
+
+    mostrarTabla() {
+        $("table").show();
+    }
+
+    ocultarLista() {
+        $("ul").hide();
+    }
+
+    mostrarLista() {
+        $("ul").show();
+    }
+
+    traducirTituloIngles() {
+        $("#titulo").text("Bill Gates, one of the biggest contributors to software development:");
+        
+    }
+
+    traducirTituloEspañol() {
+        $("#titulo").text("Bill Gates, uno de los mayores contribuyentes a la informática:");
+    }
+
+    
+}
+
+var gestor = new GestorWeb();
