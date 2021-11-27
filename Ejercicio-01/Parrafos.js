592800385a913116292e2dc04476db652f95533e
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-27 13:44:20 +0100

    Update Parrafos.js

--- Ejercicio-01/Parrafos.js
+++ Ejercicio-01/Parrafos.js
@@ -1,10 +1,6 @@
 document.write("<section>");
 document.write("<h2>Datos completos de la asignatura: </h2>");
 
-document.write("</p>");
-document.write("<p>Universidad: ");
-document.write(asignatura.universidad);
-document.write("</p>");
 document.write("<p>Curso actual: ");
 document.write(asignatura.curso);
 document.write("</p>");
