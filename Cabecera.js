db654bb6b4607c6e47f96335ba045dd8d057da2e
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-27 13:10:42 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,12 @@
+var asignatura = new Object();
+
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-2021";
+asignatura.alumno = "Oscar Perez Arruñada";
+asignatura.email = "uo265488@uniovi.es";
+
+
+    
---
+++ Parrafos.js
@@ -0,0 +1,17 @@
+document.write("<section>");
+document.write("<h2>Datos completos de la asignatura: </h2>");
+
+document.write("</p>");
+document.write("<p>Universidad: ");
+document.write(asignatura.universidad);
+document.write("</p>");
+document.write("<p>Curso actual: ");
+document.write(asignatura.curso);
+document.write("</p>");
+document.write("<p>Alumno: ");
+document.write(asignatura.alumno);
+document.write("</p>");
+document.write("<p>e-mail: ");
+document.write(asignatura.email);
+document.write("</p>");
+document.write("</section>");
