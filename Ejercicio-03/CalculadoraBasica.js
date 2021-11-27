3c9f4a581b73fbd6485074a7a8727b55d28ec4d0
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-27 16:53:14 +0100

    Add files via upload

--- Ejercicio-03/CalculadoraBasica.js
+++ Ejercicio-03/CalculadoraBasica.js
@@ -10,114 +10,85 @@ class Calculadora {
 
     digitos(num) {
         this.pantalla += String(num);
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
 
     punto() {
         this.pantalla += '.';
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
 
     suma() {
         this.pantalla += " + ";
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
 
     resta() {
         this.pantalla += " - ";
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
     
     multiplicacion() {
         this.pantalla += ' * ';
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
 
     division() {
         this.pantalla += ' / ';
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
 
     mrc() {
         this.pantalla = String(this.memory);
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
 
     mMas() {
-        this.memory = Number(this.memory) + eval(this.pantalla) ;
+        if(String(this.pantalla) != "") {
+            this.memory = Number(this.memory) + eval(this.pantalla) ;
+        } else {
+            alert('No se puede guardar una expresión vacía. ');
+        }
     }
 
     mMenos() {
-        this.memory = Number(this.memory) - eval(this.pantalla);
+        if(String(this.pantalla) != "") {
+            this.memory = Number(this.memory) - eval(this.pantalla) ;
+        } else {
+            alert('No se puede guardar una expresión vacía. ');
+        }
     }
 
     borrar() {
         this.pantalla = this.pantalla.substr(0, this.pantalla.length - 1);
-        document.getElementById("pantalla").value = String(this.pantalla);
+        this.showPantalla();
     }
 
     isValidFormat() { 
-        if (this.pantalla.length == 0) {
-            alert('La pantalla no puede estar vacía. ');
-        } else if (this.pantalla[0] == '0' && DIGITS.indexOf(this.pantalla[1]) != -1) {
-            alert('Un dígito no puede empezar por "0". ')
-        } else if(DIGITS.indexOf(this.pantalla.substr(this.pantalla.length-1, 1)) == -1 && this.pantalla.substr(this.pantalla.length-1, 1)!= '.') {
-            alert('La fórmula no puede terminar en un operador. ')
-        } else if (this.hasDoublePoint()) {
-            alert('Formato de número inválido: un número tiene más de un punto.');
-        } else if (this.isPointAlone()) {
-            alert('Formato de número inválido: el punto no tiene digitos a la derecha ni a la izquierda. ');
-        } else if (Number.isNaN(this.pantalla)) {
-            alert("El resultado de la fórmula no es un número. ");
-        } else if (this.pantalla.substr(0, 3) == 'NaN') {
-            alert("La fórmula marcada no es un número. ");
-        } else {
-            //falta chekiar los octales
-            //entrada por teclado
-            return true;
-        }        
+       try {
+        this.pantalla = String(eval(this.pantalla));
+       } catch(e) {
+           this.pantalla = "";
+           document.getElementById("pantalla").value = String("SYNTAX ERROR");
+           return false;
+       }
+       return true;
+    }
+
+    showPantalla() {
+        document.getElementById("pantalla").value = String(this.pantalla);
     }
 
     igual () {
-        if (this.isValidFormat()) {
+        if( this.pantalla == "") {
+            alert('No se puede operar con una expresión vacía. ')
+        }else if (this.isValidFormat()) {
             this.pantalla = String(eval(this.pantalla));
-            document.getElementById("pantalla").value = String(this.pantalla);
+            this.showPantalla();
+            
         }
-    }
 
-    hasDoublePoint() {
-        var counter = 0;
-        for(var i = 0; i < this.pantalla.length; i++) {
-            if(this.pantalla[i] == '.') {
-                counter++;
-            }
-            if(counter == 2) {
-                return true;
-            }
-            if(OPERATORS.indexOf(this.pantalla[i]) != -1) {
-                counter = 0;
-            }
-        }
-        return false;
     }
-
-    isPointAlone() {
-        if (this.pantalla.length == 1 && this.pantalla[0] == '.') {
-            return true;
-        } else if(this.pantalla.length == 2 && this.pantalla[0] == '.' && DIGITS.indexOf(this.pantalla[1]) == -1) {
-            return true;
-        } else {
-            for(var i = 0; i < this.pantalla.length; i++) {
-                if(this.pantalla[i] == '.') {
-                    if(DIGITS.indexOf(this.pantalla[i - 1]) == -1 && DIGITS.indexOf(this.pantalla[i + 1]) == -1) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
 }
 
 var calculadora = new Calculadora();
