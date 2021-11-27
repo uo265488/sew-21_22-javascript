4d2946f9343fd5d71eacd74573966293194b5568
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-27 17:44:54 +0100

    Add files via upload

--- Ejercicio-03/CalculadoraBasica.js
+++ Ejercicio-03/CalculadoraBasica.js
@@ -6,6 +6,44 @@ class Calculadora {
     constructor() {
         this.memory = 0;
         this.pantalla = String('');
+
+        document.addEventListener('keydown', (event) => {
+            if ( event.keyCode == 13) {
+                calculadora.igual();
+            } else if ( event.keyCode == 48) {
+                calculadora.digitos(0);
+            }  else if ( event.keyCode == 49) {
+                calculadora.digitos(1);
+            } else if ( event.keyCode == 50) {
+                calculadora.digitos(2);
+            } else if ( event.keyCode == 51) {
+                calculadora.digitos(3);
+            } else if ( event.keyCode == 52) {
+                calculadora.digitos(4);
+            } else if ( event.keyCode == 53) {
+                calculadora.digitos(5);
+            } else if ( event.keyCode == 54) {
+                calculadora.digitos(6);
+            } else if ( event.keyCode == 55) {
+                calculadora.digitos(7);
+            } else if ( event.keyCode == 56) {
+                calculadora.digitos(8);
+            } else if ( event.keyCode == 57) {
+                calculadora.digitos(9);
+            } else if ( event.keyCode == 8) {
+                calculadora.borrar();
+            } else if ( event.keyCode == 106) {
+                calculadora.multiplicacion();
+            } else if ( event.keyCode == 107) {
+                calculadora.suma();
+            } else if ( event.keyCode == 109) {
+                calculadora.resta();
+            } else if ( event.keyCode == 110) {
+                calculadora.punto();
+            } else if ( event.keyCode == 111) {
+                calculadora.division();
+            }
+          });
     }
 
     digitos(num) {
