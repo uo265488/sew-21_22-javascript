d8053614011ee38643e2b9a982984d8b0727cbad
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-28 14:08:43 +0100

    Add files via upload

--- Ejercicio-07/Ejercicio7.js
+++ Ejercicio-07/Ejercicio7.js
@@ -36,53 +36,9 @@ class GestorWeb {
 
     traducirTituloEspañol() {
         $("#titulo").text("Bill Gates, uno de los mayores contribuyentes a la informática:");
-    }   
-
-    añadirFoto() {
-        $("#fotos").before('<img src="multimedia/BillGates.png" alt="Bill Gates."  />');
     }
 
-    borrarH3() {
-        $('h3').remove();
-    }
-
-    borrarH2() {
-        $('h2').remove();
-    }
-
-    añadirBotonEliminarTodo() {
-        $("#borrarTodo").after('<input type="button" value="Borrar todo" onClick= gestor.borrarSecciones()>');
-    }
-
-    borrarSecciones() {
-        $("section").remove();
-    }
-
-    recorrarYMostrarPadres() {
-        $("*", document.body).each(function() {
-            var etiquetaPadre = $(this).parent().get(0).tagName;
-            $(this).prepend(document.createTextNode( "Etiqueta padre : <"  + etiquetaPadre + "> elemento : <" + $(this).get(0).tagName +"> valor: "));
-        });
-    }
-
-    contarFilasYColumnas() {
-        var numFilas = 0;
-        var numColumnas = 0;
-        $("tr").each(function() {
-            numFilas += 1;
-        });
-
-        $("td").each(function() {
-            numColumnas += 1;
-        });
-
-        $("th").each(function() {
-            numColumnas += 1;
-        });
-
-        $("#recuento").after("<p> La tabla tiene " + numFilas + " filas y " 
-        + String(numColumnas / numFilas) + " columnas. </p>")
-    }
+    
 }
 
 var gestor = new GestorWeb();
