147e7ab6cb6e43f337fdf57df9a6dae1b9a1df74
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-28 23:49:50 +0100

    Add files via upload

---
+++ Ejercicio-11/Tarea-4/Ejercicio11.js
@@ -0,0 +1,15 @@
+// TO MAKE THE MAP APPEAR YOU MUST
+	// ADD YOUR ACCESS TOKEN FROM
+	// https://account.mapbox.com
+    mapboxgl.accessToken = 'pk.eyJ1IjoidW8yNjU0ODgiLCJhIjoiY2t3YjJzNWF6MGI5djJucWtqc3p4anFrMSJ9.SdDNps_DxnoQxgDxfKERsw';
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-5.84476, 43.36029], // starting position [lng, lat]
+        zoom: 13// starting zoom
+    });
+    
+    const marker1 = new mapboxgl.Marker()
+           .setLngLat([-5.84476, 43.36029])
+           .addTo(map);
