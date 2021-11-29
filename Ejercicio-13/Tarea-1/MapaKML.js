4ea82099db4ec28cde0559db9a9b4eba242c64dd
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-11-29 02:53:35 +0100

    Add files via upload

--- Ejercicio-13/Tarea-1/MapaKML.js
+++ Ejercicio-13/Tarea-1/MapaKML.js
@@ -53,7 +53,7 @@ class MyFileReader {
                 //El evento "onload" se lleva a cabo cada vez que se completa con éxito una operación de lectura
                 //La propiedad "result" es donde se almacena el contenido del archivo
                 //Esta propiedad solamente es válida cuando se termina la operación de lectura
-                document.querySelector('section:last-child').innerHTML = lector.result;
+                document.querySelector('section:last-child').innerHTML = "Contenido del KML: " + lector.result;
             }      
             lector.readAsText(archivo);
         } else {
