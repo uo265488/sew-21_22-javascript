a4af1e17aee2cb484ae1a8f38d567fbe86f46595
uo265488 <78318693+uo265488@users.noreply.github.com>
2021-12-01 03:17:28 +0100

    Add files via upload

--- Ejercicio-11/Tarea-4/Ejercicio11.js
+++ Ejercicio-11/Tarea-4/Ejercicio11.js
@@ -1,15 +1,26 @@
 // TO MAKE THE MAP APPEAR YOU MUST
 	// ADD YOUR ACCESS TOKEN FROM
 	// https://account.mapbox.com
-    mapboxgl.accessToken = 'pk.eyJ1IjoidW8yNjU0ODgiLCJhIjoiY2t3YjJzNWF6MGI5djJucWtqc3p4anFrMSJ9.SdDNps_DxnoQxgDxfKERsw';
-
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-5.84476, 43.36029], // starting position [lng, lat]
-        zoom: 13// starting zoom
-    });
+mapboxgl.accessToken = 'pk.eyJ1IjoidW8yNjU0ODgiLCJhIjoiY2t3YjJzNWF6MGI5djJucWtqc3p4anFrMSJ9.SdDNps_DxnoQxgDxfKERsw';
     
-    const marker1 = new mapboxgl.Marker()
-           .setLngLat([-5.84476, 43.36029])
-           .addTo(map);
+class Initializer {
+    constructor() {
+    }
+   
+    initialize() {
+        this.map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [-5.84476, 43.36029], // starting position [lng, lat]
+            zoom: 13// starting zoom
+        });
+        
+        this.marker1 = new mapboxgl.Marker()
+               .setLngLat([-5.84476, 43.36029])
+               .addTo(map);
+    }
+}
+
+var initializer = new Initializer();
+
+  
